39cc01a7cec1a76ba9544e2dcfdf9b0f65921cd8
Defense-geek <mrchowdary1700@gmail.com>
2024-10-09 16:05:16 +0530

    final

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return (
+    <Image
+      height={130}
+      width={130}
+      alt="logo"
+      src="/https://yt3.ggpht.com/v5yebiyShCTNr18fnhq_oWHjQ24rAcrkexosBb8rFA8HI3elXOOSzUKfqrO8IlE_HaC7mgU0sw=s88-c-k-c0x00ffffff-no-rj"
+    />
+  )
+}
